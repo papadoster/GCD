d49ef8b261db239a6224ca4cc02ce4c50a598062
Aleksandr Karpov <papadoster13@gmail.com>
2023-01-13 15:07:13 +0100

    Initial Commit

---
+++ GCD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by Александр Карпов on 13.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
